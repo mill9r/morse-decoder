1afa0e54e68a0c0d4f63a2cca4f8c073dfa07f7c
mill9r <mill9r@gmail.com>
2019-09-25 22:41:02 +0300

    feat: solution for morse task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const SPACE =' ';
+    let processedMorseMessage = processMorse(deleteUnusedZeros(splitOnChunks(expr)));
+    return processedMorseMessage.reduce((acc,elem)=>{
+        if(elem === SPACE){
+            return acc + ' ';
+        }
+        return acc + MORSE_TABLE[elem];
+    },'')
+}
+
+function splitOnChunks(str){
+    let outputChunk =[];
+    let chunks = str.length/10;
+    for (let i = 0; i < chunks; i++) {
+        outputChunk.push(str.slice(i*10,10+i*10))
+    }
+    return outputChunk;
+}
+
+function deleteUnusedZeros(arr){
+    return arr.reduce((acc,elem)=>{
+        while (elem.charAt(0)==='0'){
+            elem = elem.substr(1)
+        }
+        acc.push(elem);
+        return  acc
+    },[])
+}
+
+function processMorse(arr) {
+    const SPACE = '**********';
+    return arr.reduce((acc,elem)=>{
+        if(elem === SPACE){
+            acc.push(' ');
+        }else{
+            let replacedWithPoint = elem.replace(/10/g,'.');
+            let replacedWithDash = replacedWithPoint.replace(/11/g,'-');
+            acc.push(replacedWithDash);
+        }
+        return acc
+    },[])
 }
 
 module.exports = {
     decode
-}
+};
